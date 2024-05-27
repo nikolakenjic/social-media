ef991bce7cd89395044f4dada8c304d404002eb5
Nikola Kenjic <nikola.nico89@gmail.com>
2024-05-27 20:27:31 +0200

    login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,14 @@
 import { StatusCodes } from 'http-status-codes';
 import User from './../models/UserModel.js';
+import { comparePassword, hashedPassword } from '../utils/passwordEncrypted.js';
+import AppError from '../utils/appError.js';
 
 const signup = async (req, res, next) => {
   try {
+    const hashPwd = await hashedPassword(req.body.password);
+
+    req.body.password = hashPwd;
+
     const user = await User.create(req.body);
 
     res
@@ -14,7 +20,28 @@ const signup = async (req, res, next) => {
 };
 
 const login = async (req, res, next) => {
-  res.send('login');
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new AppError('Can not find the user', 404);
+    }
+
+    const isValidUser =
+      user && (await comparePassword(password, user.password));
+
+    if (!isValidUser) {
+      throw new AppError('Password dont match', 401);
+    }
+
+    res
+      .status(StatusCodes.OK)
+      .json({ message: 'Successfully created profile', user });
+  } catch (err) {
+    next(err);
+  }
 };
 
 const logout = async (req, res, next) => {
