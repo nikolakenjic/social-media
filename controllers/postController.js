1a996a8fe8fb62f707bfe283122c8c97e048c98b
Nikola Kenjic <nikola.nico89@gmail.com>
2024-05-28 18:50:46 +0200

    post

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,9 +1,45 @@
 import { StatusCodes } from 'http-status-codes';
 import Post from './../models/PostModel.js';
+import User from './../models/UserModel.js';
 import AppError from '../utils/appError.js';
 
 const getTimeline = async (req, res, next) => {
-  res.send('getTimeline');
+  try {
+    const currentUser = await User.findById(req.body.userId);
+
+    const userPost = await Post.find({ userId: currentUser._id });
+    const friendsPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+
+    const allPosts = userPost.concat(...friendsPosts);
+    // if (!userPost) {
+    //   throw new AppError('Post not found', StatusCodes.NOT_FOUND);
+    // }
+
+    res.status(StatusCodes.OK).json({ message: 'Success', allPosts });
+    // res.send('user');
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
+
+const getSinglePost = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      throw new AppError('Post not found', StatusCodes.NOT_FOUND);
+    }
+
+    res.status(StatusCodes.OK).json({ message: 'Success', post });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 };
 
 const createPost = async (req, res, next) => {
@@ -70,19 +106,32 @@ const deletePost = async (req, res, next) => {
   }
 };
 
-const likePost = async (req, res, next) => {
-  res.send('likePost');
-};
+const likeDislikePost = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id);
 
-const dislikePost = async (req, res, next) => {
-  res.send('dislikePost');
+    if (!post) {
+      throw new AppError('Post not found', StatusCodes.NOT_FOUND);
+    }
+
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(StatusCodes.OK).json({ message: 'Post liked' });
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(StatusCodes.OK).json({ message: 'Post disliked' });
+    }
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 };
 
 export {
   getTimeline,
+  getSinglePost,
   createPost,
   updatePost,
   deletePost,
-  likePost,
-  dislikePost,
+  likeDislikePost,
 };
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,17 +1,19 @@
 import { Router } from 'express';
 import {
   getTimeline,
+  getSinglePost,
   createPost,
   updatePost,
   deletePost,
-  likePost,
-  dislikePost,
+  likeDislikePost,
 } from '../controllers/postController.js';
 
 const router = Router();
 
 router.route('/').get(getTimeline).post(createPost);
 
-router.route('/:id').patch(updatePost).delete(deletePost);
+router.route('/:id').get(getSinglePost).patch(updatePost).delete(deletePost);
+
+router.route('/:id/like').put(likeDislikePost);
 
 export default router;
