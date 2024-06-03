6ea0793fdb6490ec8f56c753da014140f82718f9
Nikola Kenjic <nikola.nico89@gmail.com>
2024-06-03 20:54:27 +0200

    client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,29 @@
-import { Home } from './pages';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Home, Login, Profile, Register } from './pages';
+
+const router = createBrowserRouter([
+  // {
+  //   path: '',
+  //   element: <RootLayout />,
+  //   errorElement: <ErrorPage />,
+  //   children: [
+  //     { index: true, element: <HomePage /> },
+  //     {
+  //       path: ':id',
+  //       element: <DetailPage />,
+  //       loader: detailLoader,
+  //     },
+  //   ],
+  // },
+
+  { path: '/', element: <Home /> },
+  { path: '/login', element: <Login /> },
+  { path: '/register', element: <Register /> },
+  { path: '/profile/:username', element: <Profile /> },
+]);
 
 const App = () => {
-  return (
-    <>
-      <Home />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
