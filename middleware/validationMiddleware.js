966d58ec224a17022cd72e6c2d912d7172c1f449
Nikola Kenjic <nikola.nico89@gmail.com>
2024-06-13 19:51:45 +0200

    validation

---
+++ middleware/validationMiddleware.js
@@ -0,0 +1,55 @@
+import { body, validationResult } from 'express-validator';
+import User from '../models/UserModel.js';
+import AppError from '../utils/appError.js';
+import { StatusCodes } from 'http-status-codes';
+
+const validatorWithErrors = (validationValues) => {
+  return [
+    validationValues,
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        const errorMsgs = errors.array().map((err) => err.msg);
+        console.log(errorMsgs);
+
+        throw new AppError(errorMsgs);
+      }
+
+      next();
+    },
+  ];
+};
+
+// USER VALIDATION
+
+// Register
+
+export const validationUserRegister = validatorWithErrors([
+  body('username', 'Username is required').trim().notEmpty(),
+  body('email', 'Please enter a valid email like: test@test.com')
+    .trim()
+    .isEmail()
+    .custom(async (value) => {
+      const userDoc = await User.findOne({ email: value });
+      if (userDoc) {
+        throw new AppError('Email already exist', StatusCodes.CONFLICT);
+      }
+    }),
+  body('password', 'Password is required')
+    .trim()
+    .notEmpty()
+    .isLength({ min: 6, max: 30 })
+    .withMessage('Password must have min 6 and max 30 character.'),
+]);
+
+// Login
+export const validationUserLogin = validatorWithErrors([
+  body('email', 'Please enter a valid email like: test@test.com')
+    .trim()
+    .isEmail(),
+  body('password', 'Password is required')
+    .trim()
+    .notEmpty()
+    .isLength({ min: 6, max: 30 })
+    .withMessage('Password must have min 6 and max 30 character.'),
+]);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { signup, login, logout } from './../controllers/authController.js';
+import { validationUserRegister } from '../middleware/validationMiddleware.js';
 
 const router = Router();
 
-router.route('/signup').post(signup);
+router.route('/signup').post(validationUserRegister, signup);
 router.route('/login').post(login);
 router.route('/logout').get(logout);
 
