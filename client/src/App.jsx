d9db9cdedcb2d6eb0ed58edc7997a6c34275964f
Nikola Kenjic <nikola.nico89@gmail.com>
2024-09-03 19:17:12 +0200

    client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,37 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Navigate,
+} from 'react-router-dom';
 import { Home, Login, Profile, Register } from './pages';
 import LoginPage from './pages/LoginPage';
-
-const router = createBrowserRouter([
-  // {
-  //   path: '',
-  //   element: <RootLayout />,
-  //   errorElement: <ErrorPage />,
-  //   children: [
-  //     { index: true, element: <HomePage /> },
-  //     {
-  //       path: ':id',
-  //       element: <DetailPage />,
-  //       loader: detailLoader,
-  //     },
-  //   ],
-  // },
-
-  { index: true, element: <Home /> },
-  { path: 'login', element: <Login /> },
-  { path: 'register', element: <Register /> },
-  { path: 'profile/:username', element: <Profile /> },
-]);
+import { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
 
 const App = () => {
+  const { user } = useContext(AuthContext);
+
+  const router = createBrowserRouter([
+    // {
+    //   path: '',
+    //   element: <RootLayout />,
+    //   errorElement: <ErrorPage />,
+    //   children: [
+    //     { index: true, element: <HomePage /> },
+    //     {
+    //       path: ':id',
+    //       element: <DetailPage />,
+    //       loader: detailLoader,
+    //     },
+    //   ],
+    // },
+
+    { index: true, element: user ? <Home /> : <Register /> },
+    { path: 'login', element: user ? <Navigate to="/" /> : <Login /> },
+    { path: 'register', element: user ? <Navigate to="/" /> : <Register /> },
+    { path: 'profile/:username', element: <Profile /> },
+  ]);
+
   return <RouterProvider router={router} />;
 };
 
